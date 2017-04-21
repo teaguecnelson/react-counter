7422f49f393956f3832d3e6de0d6938f8826fdba
teaguecnelson <teaguecnelson@gmail.com>
2017-04-21 12:49:01 -0400

    yo

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const title = React.createElement(
+const { createElement } = React
+const { Render } = ReactDOM
+
+const style = {
+	backgroundColor: 'orange',
+	color: 'white',
+	fontFamily: 'verdana'
+}
+
+const title = createElement(
 	'h1',
-	{id: 'title', className: 'header'},
-	'Hello World'	
+	{id: 'title', className: 'header', style: style},
+	'Hello World Yp'	
 )
 
-ReactDOM.render(
+render(
 	title,
 	document.getElementById('react-container')
 )
